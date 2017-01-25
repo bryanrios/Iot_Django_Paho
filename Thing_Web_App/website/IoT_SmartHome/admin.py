ca83eb7bab6a5a3185bf7e88359c040d8f20c838
Mateusz Tasz <mateusz.tasz@gmail.com>
2017-01-25 20:02:08 +0100

    Added database

--- Thing_Web_App/website/IoT_SmartHome/admin.py
+++ Thing_Web_App/website/IoT_SmartHome/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Room, Temp, Light
+
+admin.site.register(Room)
+admin.site.register(Temp)
+admin.site.register(Light)
--- Thing_Web_App/website/IoT_SmartHome/models.py
+++ Thing_Web_App/website/IoT_SmartHome/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Room(models.Model):
+    description = models.CharField(max_length=250)
+    owner_name = models.CharField(max_length=250)
+    owner_surname = models.CharField(max_length=250)
+    avatar = models.FileField()
+
+    def __str__(self):
+        return 'The room belongs to ' + self.owner_name + ' ' + self.owner_surname
+
+
+class Light(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    value = models.CharField(max_length=5)
+
+    def __str__(self):
+        return 'Light state in room' + self.room.description + ' is: ' + str(self.value) + ' in time: ' + str(self.time)
+
+
+class Temp(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    value = models.FloatField()
+
+    def __str__(self):
+        return 'Temperature in room' + self.room.description + ' is: ' + str(self.value) + ' in time: ' + str(self.time)
